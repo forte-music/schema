f9c08b18843c199e123cc2dff567cbcaa5c021f0
Martin Charles <martincharles07@gmail.com>
2018-01-25 17:04:12 -0500

    Moved Fragments
    Also added failing album test.

--- tests/tests/album.js
+++ tests/tests/album.js
@@ -1,8 +1,40 @@
+import gql from 'graphql-tag';
+import { print } from 'graphql/language/printer';
+
+import AlbumFields from './fragments/AlbumFields.graphql';
+import ArtistFields from './fragments/ArtistFields.graphql';
+import SongFields from './fragments/SongFields.graphql';
+
 import client from '../client';
 
-it('should calculate the duration for an album', async () => {
-  const variables = { albumId: 'album:4' };
+const variables = { albumId: 'album:4' };
+it('should get information about an album by id', async () => {
+  const query = gql`
+    query($albumId: ID!) {
+      album(id: $albumId) {
+        ...AlbumFields
 
+        artist {
+          ...ArtistFields
+        }
+
+        songs {
+          ...SongFields
+        }
+      }
+    }
+
+    ${AlbumFields}
+    ${ArtistFields}
+    ${SongFields}
+  `;
+
+  const { album } = await client.request(print(query), variables);
+
+  expect(album).toMatchSnapshot();
+});
+
+it('should calculate the duration for an album', async () => {
   const { album } = await client.request(
     `
       query($albumId: ID!) {
