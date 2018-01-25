b58ece03ec7f7c9c4a167e10b241e4797e21533d
Martin Charles <martincharles07@gmail.com>
2018-01-25 18:42:58 -0500

    Fixed Schema Test
    It wasn't reporting some changes.

--- tests/tests/schema.js
+++ tests/tests/schema.js
@@ -5,8 +5,10 @@ import {
   buildClientSchema,
 } from 'graphql/utilities';
 
-import schema from '../../schema.graphql';
+import schemaAST from '../../schema.graphql';
 import client from '../client';
+
+const realSchema = buildASTSchema(schemaAST);
 
 const getSchema = async client => {
   const data = await client.request(introspectionQuery);
@@ -15,13 +17,35 @@ const getSchema = async client => {
   return schema;
 };
 
-it("shouldn't have any breaking changes against the schema", async () => {
-  const expectedSchema = buildASTSchema(schema);
-  const actualSchema = await getSchema(client);
+it('should implement the schema and nothing more', async () => {
+  const remoteSchema = await getSchema(client);
 
-  const changes = findBreakingChanges(actualSchema, expectedSchema).filter(
-    change => change.type !== 'DIRECTIVE_LOCATION_REMOVED'
-  );
+  const ignoreChangesPredicate = change =>
+    ['DIRECTIVE_LOCATION_REMOVED', 'DIRECTIVE_REMOVED'].indexOf(change.type) ===
+    -1;
 
-  expect(changes).toEqual([]);
+  const remoteBreakingChanges = findBreakingChanges(
+    remoteSchema,
+    realSchema
+  ).filter(ignoreChangesPredicate);
+
+  // Shows breaking changes when going from the remote schema to the real
+  // schema. These changes can be resolved by doing them.
+  expect(remoteBreakingChanges).toEqual([]);
+
+  const schemaBreakingChanges = findBreakingChanges(
+    realSchema,
+    remoteSchema
+  ).filter(ignoreChangesPredicate);
+
+  // Shows breaking changes when going from the real schema to the remote
+  // schema. To resolve these changes, undo them. For example if the change
+  // says:
+  //
+  //     Query.albums arg input has changed type from ConnectionQuery to
+  //     ConnectionQuery!
+  //
+  // To fix it, undo the change by changing ConnectionQuery! to
+  // ConnectionQuery on the remote.
+  expect(schemaBreakingChanges).toEqual([]);
 });
