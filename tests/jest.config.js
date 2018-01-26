1e5da75c45ff105406e056e8bf345569bc7cdc32
Martin Charles <martincharles07@gmail.com>
2018-01-25 20:24:54 -0500

    Fixed Broken Jest Test Configuration

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
     '\\.(gql|graphql)$': 'jest-transform-graphql',
   },
   testMatch: ['<rootDir>/tests/*.js'],
+
+  // Jest assumes it isn't running from inside node_modules.
+  // https://github.com/facebook/jest/issues/2145
+  haste: {
+    providesModuleNodeModules: ['.*'],
+  },
+  transformIgnorePatterns: ['<rootDir>/../node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/../node_modules/'],
 };
