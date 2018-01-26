e3b8fd224b873eb61696a4859ee001198b674c81
Martin Charles <martincharles07@gmail.com>
2018-01-26 16:10:51 -0500

    Added createPlaylist Test

--- tests/tests/playlist.js
+++ tests/tests/playlist.js
@@ -37,3 +37,37 @@ it('should get a playlist by id', async () => {
   const resp = await client.request(print(query), variables);
   expect(resp).toMatchSnapshot();
 });
+
+it('should create a new playlist', async () => {
+  const variables = {
+    name: 'Test Created Playlist',
+    songs: ['song:4:6', 'song:4:16', 'song:4:19'],
+  };
+
+  const mutation = gql`
+    mutation($name: String!, $songs: [ID!]!) {
+      createPlaylist(name: $name, songs: $songs) {
+        ...PlaylistFields
+
+        items(input: { limit: -1 }) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    }
+
+    ${PlaylistFields}
+  `;
+
+  const { createPlaylist } = await client.request(print(mutation), variables);
+
+  expect(createPlaylist).toMatchObject({
+    name: variables.name,
+    items: {
+      edges: variables.songs.map(id => ({ node: { id } })),
+    },
+  });
+});
